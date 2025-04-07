a28fe8d62349698a9e4bd532d342be2f3ce0afe4
carlova755 <carlosandresvanegaspere@gmail.com>
2025-04-07 11:51:56 -0500

    Segmentacion del codigo

--- holaPOO.py
+++ holaPOO.py
@@ -1,9 +1,4 @@
-class Saludo:
-    def __init__(self,mensaje):
-        self.mensaje = mensaje
-    
-    def Mostar_mensaje(self):
-        print(self.mensaje)
-        
+from holaClass import Saludo
+
 saludo = Saludo("hola mundo")
 saludo.Mostar_mensaje()
